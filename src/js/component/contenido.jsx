cd7a70a5fc50d112e69c415552fb71b081b99d5e
Pablo <cont.pab@gmail.com>
2022-02-19 02:15:05 -0300

    Se agrega metodo para guardar lista actualizada

--- src/js/component/contenido.jsx
+++ src/js/component/contenido.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// const hoverHandler = (e) => {
+// 	console.log("onMouseEnter");
+// 	//e.target.style.display = "none";
+// 	let elemento = document.getElementById("eliminar");
+// 	elemento.style.display = "flex";
+// 	console.log(elemento);
+// };
+// const outHandler = () => {
+// 	console.log("onMouseLeave");
+// 	let elemento = document.getElementById("eliminar");
+// 	elemento.style.display = "none";
+// };
+// onMouseEnter={hoverHandler}
+// onMouseLeave={outHandler}>
 
 const Contenido = ({ lista, index, borrarElemento }) => {
 	return (
 		<>
-			<div className="contenedor ">
+			<div className="contenedor">
 				<div className="texto">
 					<h1 className="">{lista}</h1>
-					<hr />
-				</div>
 
-				<div className="botonCerrar">
-					<div
-						className="btn-close"
-						aria-label="Close"
-						onClick={() => borrarElemento(index)}></div>
+					<div className="botonCerrar">
+						<div
+							id="eliminar"
+							className="btn-close"
+							aria-label="Close"
+							onClick={() => borrarElemento(index)}></div>
+					</div>
 				</div>
 			</div>
+			<hr />
 		</>
 	);
 };
--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -7,38 +7,74 @@ import Contenido from "./contenido.jsx";
 //create your first component
 const List = () => {
 	const [lista, setLista] = useState([]);
-	const [listas, setListas] = useState([]);
-	console.log(listas);
-	let total = listas.length;
+	const [listaTarea, setListaTarea] = useState([]);
+	let total = listaTarea.length;
 	let cantidad = total > 0 ? `${total} item left` : "";
-	const capturarTexto = (e) => setLista({ [e.target.name]: e.target.value });
+	const capturarTexto = (e) =>
+		setLista({ [e.target.name]: e.target.value, ["done"]: false });
 	//const handleClick = (e) => console.log("click click");
 
-	// const handleClick = (e) => {
-	// 	setListas([...listas, lista]);
-	// };
+	console.log(total, "total de elementos");
 
-	const borrarElemento = (indice) => {
-		const newTodos = [...listas];
-		newTodos.splice(indice, 1);
-		setListas(newTodos);
-	};
-	function agregarLista(e) {
-		if (e.charCode == 13) {
-			setListas([...listas, lista]);
-		}
-		// if (e.keyCode == 13) {
-		// 	alert("Enter... (KeyDown, use keyCode)");
-		// }
-	}
 	useEffect(() => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr")
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/pablocontrerasc"
+		)
 			.then((response) => response.json())
-			.then((listas) => setListas(listas));
+			.then((data) => setListaTarea(data))
+			.catch((error) => console.log(error));
 	}, []);
 
-	// var map = new Map(Object.entries(data));
-	// console.log(map);
+	if (total == undefined) {
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/pablocontrerasc",
+			{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify([]),
+			}
+		)
+			.then((response) => response.json())
+			.catch((error) => console.log(error));
+	}
+
+	const borrarElemento = (indice) => {
+		console.log([...listaTarea], "lista tarea anterior");
+		let nuevaListaTarea = [...listaTarea];
+
+		nuevaListaTarea.splice(indice, 1);
+		setListaTarea(nuevaListaTarea);
+		console.log(nuevaListaTarea, "nueva lista");
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/pablocontrerasc",
+			{
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(nuevaListaTarea),
+			}
+		)
+			.then((res) => res.json())
+			.catch((error) => console.error("Error:", error))
+			.then((response) => console.log("Success:", response));
+	};
+
+	function agregarLista(e) {
+		if (e.charCode == 13) {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/pablocontrerasc",
+				{
+					method: "PUT",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify([...listaTarea, lista]),
+				}
+			)
+				.then((response) => response.json())
+				.catch((error) => console.log(error));
+			setListaTarea([...listaTarea, lista]);
+			console.log(setListaTarea);
+		}
+	}
 
 	return (
 		<>
@@ -49,20 +85,35 @@ const List = () => {
 					<div className="label">
 						<input
 							type="text"
-							name="lista"
+							name="label"
 							onChange={capturarTexto}
 							onKeyPress={agregarLista}
 						/>
 						<hr />
 					</div>
+
 					<div className="">
-						{listas.map((value, index) => (
+						{/* {total !== undefined ? (
+							listaTarea.map((value, index) => (
+								//	<li key={index}> {value.label} </li>
+								<Contenido
+									key={index}
+									lista={value.label}
+									index={index}
+									borrarElemento={borrarElemento}
+								/>
+							))
+						) : (
+							<h1> No hay elementos</h1>
+						)} */}
+
+						{listaTarea.map((value, index) => (
 							//	<li key={index}> {value.label} </li>
 							<Contenido
 								key={index}
 								lista={value.label}
 								index={index}
-								//borrarElemento={borrarElemento}
+								borrarElemento={borrarElemento}
 							/>
 						))}
 					</div>
